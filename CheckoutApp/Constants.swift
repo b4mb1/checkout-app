a25f8ee1d77d23e39c7d07ff4ac1c75afd0cea02
Klaudyna Marciniak <klaudyna.marciniak@debijenkorf.nl>
2018-02-18 15:48:02 +0100

    Add CustomNavigationController, BasketViewController and Constants to the project.
    CustomNavigationController was added to manupilate custom barButtonItems.
    Constants.swift holds all hardcoded values in the poroject.

---
+++ CheckoutApp/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  CheckoutApp
+//
+//  Created by Klaudyna Marciniak on 18/02/2018.
+//  Copyright © 2018 Klaudyna Marciniak. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct API {
+        static let baseURL = "http://apilayer.net/api/"
+        static let key = "772e8f137945688b635343c327107d9a"
+    }
+    
+    struct ImageNames {
+        static let basket = "basket"
+        static let back = "back"
+    }
+    
+    struct Storyboards {
+        static let main = "Main"
+    }
+}
---
+++ CheckoutApp/CustomNavigationController.swift
@@ -0,0 +1,127 @@
+//
+//  CustomNavigationController.swift
+//  CheckoutApp
+//
+//  Created by Klaudyna Marciniak on 18/02/2018.
+//  Copyright © 2018 Klaudyna Marciniak. All rights reserved.
+//
+
+import UIKit
+
+class CustomNavigationController: UINavigationController {
+    
+    private var basketButton: UIButton?
+    private var basketBarButtonItem: UIBarButtonItem?
+
+    private var backButton: UIButton?
+    private var backBarButtonItem: UIBarButtonItem?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupBasketButton()
+        setupBackButton()
+        self.delegate = self
+
+    }
+
+    private func setupBasketButton() {
+        guard basketButton == nil else {
+            return
+        }
+        
+        let action = #selector(basketTapped)
+        let name = Constants.ImageNames.basket
+        basketButton = createCustomButton(withAction: action,
+                                          andImageName: name)
+
+        if let button = basketButton {
+            basketBarButtonItem = UIBarButtonItem(customView: button)
+        }
+    }
+    
+    
+    private func setupBackButton() {
+        guard backButton == nil else {
+            return
+        }
+        
+        let action = #selector(backButtonTapped)
+        let name = Constants.ImageNames.back
+        backButton = createCustomButton(withAction: action,
+                                        andImageName: name)
+
+        if let button = backButton {
+            backBarButtonItem = UIBarButtonItem(customView: button)
+        }
+    }
+    
+    private func createCustomButton(withAction action: Selector,
+                                    andImageName name: String) -> UIButton {
+        
+        let button = UIButton(type: .custom)
+        button.frame = CGRect(origin: .zero, size: CGSize(width: 25, height: 25))
+        let image = UIImage(named: name)?.withRenderingMode(.alwaysTemplate)
+        button.setImage(image, for: .normal)
+        button.contentMode = .scaleAspectFit
+        button.addTarget(self, action: action, for: .touchUpInside)
+
+        return button
+    }
+
+    private func installBasketButtonOn(_ viewController: UIViewController){
+        guard let basketItem = basketBarButtonItem else {
+            return
+        }
+            viewController.navigationItem.rightBarButtonItem = basketItem
+    }
+    
+    private func installBackButtonOn(_ viewController: UIViewController){
+        guard let backItem = backBarButtonItem else {
+            return
+        }
+        
+        viewController.navigationItem.leftBarButtonItem = backItem
+    }
+    
+    @objc func basketTapped() {
+        let basketViewController = UIStoryboard(name: Constants.Storyboards.main, bundle: nil).instantiateViewController(withIdentifier: BasketViewController.identifier)
+        self.pushViewController(basketViewController, animated: true)
+    
+    }
+    
+    @objc func backButtonTapped() {
+       self.popViewController(animated: true)
+    }
+    
+    // MARK: methods with internal access level
+    
+    internal func hideBasketButton() {
+        guard let button = basketButton else {
+            return
+        }
+        
+        UIView.transition(with: button, duration: 0.3, options: .transitionCrossDissolve, animations: {
+            button.isHidden = true
+        })
+    }
+}
+
+extension CustomNavigationController: UINavigationControllerDelegate {
+    
+    func navigationController(_ navigationController: UINavigationController,
+                              willShow viewController: UIViewController,
+                              animated: Bool) {
+       
+        backButton?.isHidden = false
+        basketButton?.isHidden = false
+        
+        installBasketButtonOn(viewController)
+        
+        guard navigationController.viewControllers.count > 1 else {
+            backButton?.isHidden = true
+            return
+        }
+        
+        installBackButtonOn(viewController)
+    }
+}
