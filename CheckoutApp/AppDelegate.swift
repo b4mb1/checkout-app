8c5bb59f5b817219e65c94231ce901caef4586d4
Klaudyna Marciniak <klaudyna.marciniak@debijenkorf.nl>
2018-02-20 11:59:41 +0100

    Remove subscription of MainViewController from AddDelegate

--- CheckoutApp/AppDelegate.swift
+++ CheckoutApp/AppDelegate.swift
@@ -17,15 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         UINavigationBar.appearance().tintColor = .black
-        
-        let store = Store.shared
-        
-        if let navigationController = window?.rootViewController as? CustomNavigationController,
-            let viewController = navigationController.viewControllers[0] as? MainViewController {
-            viewController.dispatch = store.dispatch
-            store.subscribe(viewController)
-            store.propagate()
-        }
 
         return true
     }
